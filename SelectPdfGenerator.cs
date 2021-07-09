cd82e2b648fc3d92d27e4ae100c90ca3c668258c
luizfernandomeier <lfmeier2@gmail.com>
2021-07-09 15:32:03 -0300

    Format

--- SelectPdfGenerator.cs
+++ SelectPdfGenerator.cs
@@ -1,5 +1,5 @@
 namespace PdfGenerationWebServer {
-        public class SelectPdfGenerator : IPdfGenerator {
+    public class SelectPdfGenerator : IPdfGenerator {
         public byte[] CreatePdfFromHtml(string html) {
             var renderer = new SelectPdf.HtmlToPdf();
             var pdf = renderer.ConvertHtmlString(html);
